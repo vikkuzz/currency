daafe2fb27e9f601793414651920ca1ba6abcaed
Vikkuzz <vikkuzz@vk.com>
2023-04-20 18:00:27 +0300

    test

--- src/index.js
+++ src/index.js
@@ -2,29 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-let snow = document.getElementById("snow");
-// когда кнопка нажата — находим активную вкладку и запускаем нужную функцию
-snow.addEventListener("click", async () => {
-  // получаем доступ к активной вкладке
-  let [tab] = await chrome.tabs.query({
-    active: true,
-    currentWindow: true,
-  });
-  // выполняем скрипт
-  chrome.scripting.executeScript({
-    // скрипт будет выполняться во вкладке, которую нашли на предыдущем этапе
-    target: { tabId: tab.id },
-    // вызываем функцию, в которой лежит запуск снежинок
-    function: snowFall,
-  });
-});
-
-// запускаем снег
 function snowFall() {
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+
+  let snow = document.getElementById("snow");
+  // когда кнопка нажата — находим активную вкладку и запускаем нужную функцию
+  snow.addEventListener("click", async () => {
+    // получаем доступ к активной вкладке
+    let [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    // выполняем скрипт
+    chrome.scripting.executeScript({
+      // скрипт будет выполняться во вкладке, которую нашли на предыдущем этапе
+      target: { tabId: tab.id },
+      // вызываем функцию, в которой лежит запуск снежинок
+      function: snowFall,
+    });
+  });
+
   let panel = document.querySelector(".call-put-block");
 
   root.render(
