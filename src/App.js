660a07b5c6fef0454670d594ccf1ac020c776793
Vikkuzz <vikkuzz@vk.com>
2023-04-20 17:58:00 +0300

    index js

--- src/App.js
+++ src/App.js
@@ -22,31 +22,6 @@ function App({ panel }) {
     return () => {
       clearInterval(timer);
     };
-
-    // получаем доступ к кнопке
-    let snow = document.getElementById("snow");
-    // когда кнопка нажата — находим активную вкладку и запускаем нужную функцию
-    snow.addEventListener("click", async () => {
-      // получаем доступ к активной вкладке
-      let [tab] = await chrome.tabs.query({
-        active: true,
-        currentWindow: true,
-      });
-      // выполняем скрипт
-      chrome.scripting.executeScript({
-        // скрипт будет выполняться во вкладке, которую нашли на предыдущем этапе
-        target: { tabId: tab.id },
-        // вызываем функцию, в которой лежит запуск снежинок
-        function: snowFall,
-      });
-    });
-
-    // запускаем снег
-    function snowFall() {
-      let panel = document.querySelector(".call-put-block");
-      const btn = document.createElement("button");
-      panel.appendChild(btn);
-    }
   }, []);
 
   useEffect(() => {
@@ -70,7 +45,6 @@ function App({ panel }) {
   }, [time]);
 
   useEffect(() => {
-    const panel = document.querySelector(".call-put-block");
     let count = 0;
     console.log(panel);
 
