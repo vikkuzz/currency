4875d966cb6cff9e0e4c4f4ce920c8d6fcd22f30
Vikkuzz <vikkuzz@vk.com>
2023-04-20 17:38:31 +0300

    get approov

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import eur_usd_1min from "./datas/eur_usd_1min";
 import { parseDataString, getCurrentTime } from "./utils/functions";
 import { useState } from "react";
 
-function App() {
+function App({ panel }) {
   const [data, setData] = useState([]);
   const [partData, setPartData] = useState([]);
   const [time, setTime] = useState(getCurrentTime());
@@ -22,6 +22,31 @@ function App() {
     return () => {
       clearInterval(timer);
     };
+
+    // получаем доступ к кнопке
+    let snow = document.getElementById("snow");
+    // когда кнопка нажата — находим активную вкладку и запускаем нужную функцию
+    snow.addEventListener("click", async () => {
+      // получаем доступ к активной вкладке
+      let [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+      // выполняем скрипт
+      chrome.scripting.executeScript({
+        // скрипт будет выполняться во вкладке, которую нашли на предыдущем этапе
+        target: { tabId: tab.id },
+        // вызываем функцию, в которой лежит запуск снежинок
+        function: snowFall,
+      });
+    });
+
+    // запускаем снег
+    function snowFall() {
+      let panel = document.querySelector(".call-put-block");
+      const btn = document.createElement("button");
+      panel.appendChild(btn);
+    }
   }, []);
 
   useEffect(() => {
