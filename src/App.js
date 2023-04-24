9238bc0f704c61ddc6022e62b205e5429b764a27
Vikkuzz <vikkuzz@vk.com>
2023-04-24 09:56:54 +0300

    clear my fail

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import eur_usd_1min from "./datas/eur_usd_1min";
 import { parseDataString, getCurrentTime } from "./utils/functions";
 import { useState } from "react";
 
-function App({ panel }) {
+function App() {
   const [data, setData] = useState([]);
   const [partData, setPartData] = useState([]);
   const [time, setTime] = useState(getCurrentTime());
@@ -33,89 +33,7 @@ function App({ panel }) {
       }
     }
     setPartData(result);
-  }, [data]);
-
-  useEffect(() => {
-    let result = [];
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].id == getCurrentTime()) {
-        result = data.slice(i, i + 50);
-      }
-    }
-    setPartData(result);
-  }, [time]);
-
-  useEffect(() => {
-    let count = 0;
-    console.log(partData);
-    localStorage.setItem("data", partData);
-
-    let snow = document.getElementById("root");
-    // когда кнопка нажата — находим активную вкладку и запускаем нужную функцию
-    snow.addEventListener("click", async () => {
-      // получаем доступ к активной вкладке
-      let [tab] = await chrome.tabs.query({
-        active: true,
-        currentWindow: true,
-      });
-      // выполняем скрипт
-      chrome.scripting.executeScript({
-        // скрипт будет выполняться во вкладке, которую нашли на предыдущем этапе
-        target: { tabId: tab.id },
-        // вызываем функцию, в которой лежит запуск снежинок
-        function: snowFall,
-      });
-    });
-
-    function snowFall() {
-      let panel = document.querySelector(".call-put-block");
-      let partData = localStorage.getItem("data");
-
-      console.log(panel, partData);
-
-      for (let i = 0; i < partData.length; i++) {
-        if (partData[i].вниз < "69%" && partData[i].вверх < "69%") {
-          count += 1;
-          if (count < 3) {
-            panel?.appendChild(
-              <div
-                className="height"
-                key={partData[i].id}
-                style={{
-                  border: `${
-                    partData[i].вниз < "69%" && partData[i].вверх < "69%"
-                      ? "none"
-                      : "2px solid gold"
-                  }`,
-                }}
-              >
-                <span
-                  className="elem"
-                  style={{
-                    height: `${
-                      partData[i].вниз > partData[i].вверх
-                        ? partData[i].вниз
-                        : partData[i].вверх
-                    }`,
-                    backgroundColor: `${
-                      partData[i].вниз > partData[i].вверх ? "red" : "green"
-                    }`,
-                  }}
-                >
-                  <span>{partData[i].id}:</span>
-                  <span className="text">
-                    {partData[i].вниз > partData[i].вверх
-                      ? "вниз " + partData[i].вниз
-                      : "вверх " + partData[i].вверх}
-                  </span>
-                </span>
-              </div>
-            );
-          }
-        }
-      }
-    }
-  }, [partData]);
+  }, [data, time]);
 
   return (
     <div className="App">
